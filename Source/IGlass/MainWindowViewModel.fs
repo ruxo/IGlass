744fa3d8944f7b4d25acbe5333c5a531b25ac67e
Ruxo Zheng <rz@just.sent.at>
2015-12-31 10:32:03 +0700

    Split Main Window controller into its own file.

--- Source/IGlass/MainWindowViewModel.fs
+++ Source/IGlass/MainWindowViewModel.fs
@@ -64,33 +64,6 @@ type MainWindowViewModel() as me =
 
   member __.MainWindowCommand = mainWindowCommand
 
-type MainWindowController(model: MainWindowViewModel) =
-  let mutable imageManager = ImageManager(Seq.empty)
-
-  let galleryFrom showFile (fdList: FileDesc seq) =
-    imageManager <- ImageManager(fdList)
-    model.Image <- showFile |> Option.bind imageManager.FindFileName
-    model.ImageCount <- imageManager.ImageCount
-
-  let galleryFromSingleFile(fd: FileDesc) = galleryFrom (fd.file()) [fd.getDirectory()]
-
-  let handleDrag = function
-  | DragEnter arg -> DragEventHandlers.validateDrag arg; arg.Handled <- true
-  | Drop fileList ->
-    match fileList with
-    | [] -> ()
-    | [single] -> galleryFromSingleFile single
-    | xs -> galleryFrom None xs
-  | case -> Printf.kprintf dbg "Unexpected case: %A" case
-
-  member __.Initialize() =
-    model.EventStream
-    |> Observable.filter DragEventHandlers.isDragEvents
-    |> Observable.subscribe handleDrag
-    |> ignore
-    
-  member __.SelectFileName filename = model.Image <- imageManager.FindFileName(filename)
-  member __.InitGallery = galleryFromSingleFile
 
 type ImageFromImageIndex() =
   let extractImage (path, _) =
